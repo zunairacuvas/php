3405c8d1e118fd545f59b5bdfe375732dd6c7585
zunairacuvas <146362473+zunairacuvas@users.noreply.github.com>
2023-09-29 19:04:09 +0500

    Add files via upload

---
+++ test.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Calculator</title>
+</head>
+<body>
+    <h1>Simple Calculator</h1>
+      <form method="post">
+       <input type="text" name="num1" placeholder="Enter first number"><br>
+       <input type="text" name="operator" placeholder="Enter operator (+, -, *, /)"><br>
+       <input type="text" name="num2" placeholder="Enter second number"><br>
+       <input type="submit" name="calculate" value="Calculate"><br>
+      </form>
+
+    <?php
+
+    if (isset($_POST['calculate'])) {
+        $num1 = $_POST['num1'];
+        $operator = $_POST['operator'];
+        $num2 = $_POST['num2'];
+        $result = 0;
+
+
+      switch ($operator) {
+            case '+':                            // for addition
+              $result = $num1 + $num2;
+              break;
+            case '-':                           // for substraction
+              $result = $num1 - $num2;
+              break;
+            case '*':                           //for multiplication
+                $result = $num1 * $num2;
+                break;
+            case '/':                           // for addition
+                if ($num2 != 0) {
+                    $result = $num1 / $num2;
+                } else {
+                    echo "Cannot divide by zero.";
+                }
+                break;
+
+            default:
+                echo "Invalid operator. Please use +, -, *, or /";
+                break;
+        }
+
+
+             echo "<h2>Result: $result</h2>";
+
+      }
+        ?>
+</body>
+</html>
+
